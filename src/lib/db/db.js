64c9ce362fb19634b1b47682020a94f90f09cec4
SwagataChatterjee68 <ranichatterjee21112@gmail.com>
2025-08-26 16:32:48 +0530

    add some fix

--- src/lib/db/db.js
+++ src/lib/db/db.js
@@ -1,15 +1,37 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   if (mongoose.connections[0].readyState) {
+//     console.log("Already connected to the database");
+//     return;
+//   }
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("Connected to the database");
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//   }
+// };
+// export default connectDB;
+
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  if (mongoose.connections[0].readyState) {
-    console.log("Already connected to the database");
-    return;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) throw new Error("MONGO_URI not defined");
+
+let cached = global.mongoose;
+
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => mongoose);
   }
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to the database");
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-  }
-};
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
 export default connectDB;
